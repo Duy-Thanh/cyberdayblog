e9469c69d0b2bc0a1e6377298a6a1e5839efee1f
cyberdaystudio <36120800+Duy-Thanh@users.noreply.github.com>
2023-09-29 15:16:47 +0700

    Updated

--- private/config.php
+++ private/config.php
@@ -9,11 +9,6 @@
 ob_start();
 session_start();
 
-// define('DBHOST', 'localhost');
-// define('DBUSER', 'thanhdz167');
-// define('DBPASS', 'Thanhdz2022@!');
-// define('DBNAME', 'cyberdaystudio_blog_db');
-
 $host = 'localhost';
 $user = 'thanhdz2019';
 $pass = 'Thanhdz2022@!';
--- public/js/alert.js
+++ public/js/alert.js
@@ -5,3 +5,15 @@ function show_alert_div() {
 function hide_alert_div() {
     document.getElementById('alert_div').style.display = "none";
 }
+
+var isShown = false;
+
+function show_hide() {
+    if (isShown) {
+        hide_alert_div();
+        isShown = false;
+    } else {
+        show_alert_div();
+        isShown = true;
+    }
+}
